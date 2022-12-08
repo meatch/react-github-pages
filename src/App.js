dc4d87ebda3170cafaec9c9de277b41dc92ccb6a
Mitch Gohman <meatch@me.com>
2022-12-08 07:52:42 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ const App = () => {
     return (
         <AppStyled className='App'>
             <h1>React Github Pages</h1>
+            <p>
+                Some filler text to see changes.
+                Some filler text to see changes.
+                Some filler text to see changes.
+                Some filler text to see changes.
+                Some filler text to see changes.
+                Some filler text to see changes.
+                Some filler text to see changes.
+            </p>
         </AppStyled>
     );
 }
